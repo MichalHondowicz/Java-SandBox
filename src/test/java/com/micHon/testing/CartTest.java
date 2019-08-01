548ee4ce32c0661b849fdae114ca000fee76fbfd
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-08-01 20:02:43 +0200

    assert grouping implemented

--- src/test/java/com/micHon/testing/CartTest.java
+++ src/test/java/com/micHon/testing/CartTest.java
@@ -4,14 +4,40 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class CartTest {
 
     @Test
-    void simulateMassiveOrder(){
+    void simulateMassiveOrder() {
         Cart cart = new Cart();
 
         assertTimeout(Duration.ofMillis(10), cart::simulateMassiveOrder);
+    }
+
+    @Test
+    void cartShouldBeNotEmptyWhenOrderAdded() {
+
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        cart.addOrderToCart(order);
+
+        assertThat(cart.getOrders(), allOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertAll(
+                () -> assertThat(cart.getOrders(), notNullValue()),
+                () -> assertThat(cart.getOrders(), hasSize(1)),
+                () -> assertThat(cart.getOrders(), is(not(empty()))),
+                () -> assertThat(cart.getOrders(), is(not(emptyCollectionOf(Order.class))))
+        );
     }
 }
